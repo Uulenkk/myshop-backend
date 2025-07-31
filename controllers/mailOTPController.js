a3f8a0c2e1c4a696406ce119c1841f64e8be84a7
Uulenkk <temuulent416@gmail.com>
2025-07-31 21:23:05 +0800

    Create mailOTPController.js

---
+++ controllers/mailOTPController.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'suppmate4@gmail.com',       // your email address
+    pass: 'jzyuopcnagthnwwk', // your email password or app password
+  },
+});
+
+async function sendOtpEmail(req, res) {
+  const mailOptions = {
+    from: 'suppmate4@gmail.com',
+    to: req.body.toEmail,
+    subject: 'Verify code test',
+    text: `Your OTP code is: 1234`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent!');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+export default sendOtpEmail;
